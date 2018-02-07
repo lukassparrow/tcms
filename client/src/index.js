3161f089c46ac934d55f373dc00442eb7b4bed3b
Lukas Brabec <lbrabec@redhat.com>
2018-02-07 22:32:13 +0100

    client: react routing added

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Route, Redirect } from 'react-router';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+import App from './App';
+
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/results" component={App} />
+            <Redirect from="/" to="/results" />
+        </Switch>
+    </BrowserRouter>, document.getElementById('root'));
+
 registerServiceWorker();
