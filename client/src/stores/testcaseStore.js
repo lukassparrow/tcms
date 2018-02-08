c83485c639d7d34e97eb6bfd5d2e0bc4f5023e9c
Lukas Brabec <lbrabec@redhat.com>
2018-02-08 01:36:08 +0100

    client: WIP converting to redux

--- client/src/stores/testcaseStore.js
+++ client/src/stores/testcaseStore.js
@@ -9,6 +9,50 @@ var tcsmetadata = {};
 var fakeresultsdb = {};
 var testcases = [];
 
+function tc_reducer(state = { metadata: {}, results: {}, testcases: [] }, action) {
+    switch (action.actionType) {
+        case ActionTypes.ADD_OUTCOME_RESP:
+            return {
+                ...state,
+                results: action.payload
+            }
+        case ActionTypes.REMOVE_OUTCOME_RESP:
+            return {
+                ...state,
+                results: action.payload
+            }
+        case ActionTypes.BACKEND_CALL_RESP:
+            return {
+                ...state,
+                results: {
+                    ...state.results,
+                    [action.payload.tcid]: action.payload.data
+                }
+            }
+
+        case ActionTypes.LOAD_METADATA_RESP:
+            return {
+                ...state,
+                metadata: {
+                    ...state.metadata,
+                    [action.payload.tcid]: action.payload.data
+                }
+            }
+
+        case ActionTypes.GET_TESTCASES_RESP:
+            return {
+                ...state,
+                testcases: action.payload.testcases
+            }
+
+        default:
+            return state
+            break;
+    }
+}
+
+console.log(tc_reducer({ metadata: {}, results: {}, testcases: [] }, { actionType: ActionTypes.ADD_OUTCOME_RESP }));
+
 class TestcaseStore extends EventEmitter {
     constructor() {
         super();
